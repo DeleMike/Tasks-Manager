60a402ef3b53ddb75ba1dc97176152eae165c17a
DeleMike <akindelemichael65@gmail.com>
2022-03-16 13:08:09 +0100

    connect to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
 
-const port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
 
-// start server
-app.listen(port, () => {
-   console.log(`server running on http://localhost:${port}`);
-});
+const connectDb = require('./db/connect')
+
+// app routes
+const authRoute = require('./routes/auth')
+
+const port = process.env.PORT || 3000
+
+// middleware
+// app.use(express.json())
+app.use('/api/user', authRoute)
+
+//connect to db then run server
+const start = () => {
+   try {
+      //connect db
+      connectDb(process.env.MONGO_URI)
+      app.listen(port, () => console.log(`server running on http://localhost:${port}`))
+   } catch (error) {
+      console.log('Error happened while connecting to database: '+ error);
+   } 
+}
+
+start()
