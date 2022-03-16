69f250889b26de61f74c3d190f065e104b8bf27b
DeleMike <akindelemichael65@gmail.com>
2022-03-16 11:38:18 +0100

    set up project

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// start server
+app.listen(port, () => {
+   console.log(`server running on http://localhost:${port}`);
+});
