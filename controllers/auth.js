c86b6a46118c7c5d989d22c60f70b65961572e30
DeleMike <akindelemichael65@gmail.com>
2022-03-16 16:27:45 +0100

    check for duplicate emails

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,25 @@
 const User = require('../models/User')
-
+const {
+   registerValidation,
+   loginValidation
+} = require('../configs/validation')
 /**
  * Tries to register a user
  */
 const registerUser = async (req, res) => {
+   const {
+      error
+   } = registerValidation(req.body)
+
+   if (error) return res.status(400).send(error.details[0].message)
+
+   // check if user is already in db
+   const emailExists = await User.findOne({
+      email: req.body.email
+   })
+   if (emailExists) return res.status(400).send("Email already exists.")
+
+   // create new user
    const user = new User({
       name: req.body.name,
       email: req.body.email,
