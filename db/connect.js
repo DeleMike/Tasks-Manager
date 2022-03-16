ed8ca199d18ac9e9b5389cfad337bd38b2de373c
DeleMike <akindelemichael65@gmail.com>
2022-03-16 16:27:32 +0100

    change connection access via mongodb website

--- db/connect.js
+++ db/connect.js
@@ -2,13 +2,15 @@ const mongoose = require('mongoose')
 
 /**
  * Connect to database
- */ 
+ */
 const connectDB = (url) => {
-   return mongoose.connect(url, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-   }),
-   ()=> console.log('Connected to db!');
- }
- 
- module.exports = connectDB
+  return mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    () => {
+      console.log('Connected to db!')
+    }
+}
+
+module.exports = connectDB
