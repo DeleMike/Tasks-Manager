dc2be00c77bf964ab3fdcb3ee853b14d50d2ef61
DeleMike <akindelemichael65@gmail.com>
2022-03-17 12:58:32 +0100

    verify token & authorized access to private route

---
+++ middleware/verifyToken.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+
+/**
+ * Verify user token to give access to private routes
+ */
+const authMiddleware = (req, res, next) => {
+   const token = req.header('auth-token')
+   // no token, user might not be authenticated
+   if(!token) return res.status(401).send('Access Denied')
+
+   try {
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET)
+      req.user = verified
+   } catch (error) {
+      // bad request, token might be tampered
+      return res.status(400).send('Invalid token')
+   }
+
+}
