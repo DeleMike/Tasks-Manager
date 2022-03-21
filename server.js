53ca22774480211659076cdc388ff4ebe29b0a74
DeleMike <akindelemichael65@gmail.com>
2022-03-21 16:41:37 +0100

    testing routes

--- server.js
+++ server.js
@@ -18,13 +18,8 @@ const errorHandler = require('./middleware/error-handler')
 const port = process.env.PORT || 5000
 
 // middleware
-//app.use(express.static('./public'))
-app.get('/', (req, res)=>{
-   res.sendFile(path.join(__dirname + '\\public\\index.html'))
-})
-app.get('/verify-user', (req, res) => {
-   res.sendFile(path.join(__dirname + '\\public\\verify-user.html'))
-})
+app.use(express.static('./public'))
+app.use('/verify-user', express.static('public\\verify-user.html'))
 app.use(express.json())
 
 
